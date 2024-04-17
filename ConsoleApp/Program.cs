6a4c661dadf4cc108a9ff898191db44fb378b677
Sakthi Santhosh <116057110+sakthisanthosh010303@users.noreply.github.com>
2024-04-17 07:33:26 +0100

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Sakthi (Sandy) Santhosh
+ * Created on: 11/04/2024
+ *
+ * Day-3 Challenge-5: Simple Login System
+ */
+namespace Challenge5;
+
+class Program
+{
+    static void Main()
+    {
+        const string Username = "ABC";
+        const string Password = "123";
+
+        string? username, password;
+
+        Console.Write("Enter username: ");
+        username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        password = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            return;
+
+        Console.Write("\nResult\n  Login: ");
+        Console.WriteLine((username == Username && password == Password)
+            ? "Success"
+            : "Fail");
+    }
+}
